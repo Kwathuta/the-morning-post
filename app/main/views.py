284f194ca8b7dacc5f46825562eff708a5424228
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-09-11 15:25:43 +0300

    defined views and templates

---
+++ app/main/views.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from . import main
+from ..requests import get_sources
+
+
+@main.route("/")
+def index():
+    """
+    View root page function that returns the index page
+    """
+
+    sources = get_sources()
+    title = 'News from the most Reputable Sources'
+
+    return render_template('index.html',title=title, sources = sources)
