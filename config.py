25a53e115649a8fbd556b7b53d142c0e482a2410
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-09-11 13:29:48 +0300

    setting up the configurations

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+
+class Config:
+    SOURCES_API_BASE_URL = "https://newsapi.org/v2/top-headlines/sources?apiKey={}"
+    SOURCES_API_KEY = os.environ.get("SOURCES_API_KEY")
+
+
+class ProdConfig(Config):
+    """
+    Production configuration child class
+    Args:
+        Config: The parent configuration class with general configuration settings
+    """
+
+    pass
+
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+    Args:
+        Config: The parent configuration class with general configuration settings
+    """
+
+    DEBUG = True
+
+
+config_options = {"development": DevConfig, "production": ProdConfig}
