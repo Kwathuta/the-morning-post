995ea957be4b4897b30e7e9230831541210b03b9
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-09-13 00:46:52 +0300

    added a function to get articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import main
-from ..requests import get_sources
+from ..requests import get_sources, get_articles
 
 
 @main.route("/")
@@ -13,3 +13,15 @@ def index():
     title = "News from the most Reputable Sources"
 
     return render_template("index.html", title=title, sources=sources)
+
+
+@main.route("/articles/<int:id>")
+def articles(id):
+    """
+    function that returns the articles
+    """
+
+    articles = get_articles(id)
+    title = f"{id}"
+
+    return render_template("articles.html", title=title, articles=articles)
